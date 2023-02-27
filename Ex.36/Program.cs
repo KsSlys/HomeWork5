95120ecaea25f08dc153f2dec715cb269b739929
«Ksenia» <ksenia_slys@mail.ru>
2023-02-27 17:30:59 +0300

    Ex:34,36,38

---
+++ Ex.36/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+Console.Clear();
+int[] array = GetBinArray(7, -99 , 100);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] GetBinArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+int sum = 0;
+for (int i = 0; i < array.Length; i++){
+    
+     if (i % 2 == 1)
+       sum += array [i];
+}
+Console.WriteLine($"Сумма чисел на нечетных позициях -> {sum}");
---
+++ Ex.38/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Зaдайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76 */
+Console.Clear();
+int[] array = GetBinArray(7, -99 , 100);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+
+int[] GetBinArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+int max = 0;
+
+
+for ( int i = 0; i< array.Length; i++){
+    if (array [i] > max) max = array [i];
+}
+int min = max;
+for ( int i = 0; i< array.Length; i++){
+    if (array [i] < min) min = array [i];
+}
+Console.WriteLine($"Разница между максимальным и минимальным значение = {max - min}");
